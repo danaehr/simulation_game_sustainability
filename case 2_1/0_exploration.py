a66cb50c954748c7a96c0f3d2e4916865eda2d1a
Daniel N <d.naehr@mail.de>
2026-05-14 13:15:03 +0200

    categorize by mean value thresholds

--- case 2_1/0_exploration.py
+++ case 2_1/0_exploration.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 #import copy # copy of dict
 from pandas.plotting import parallel_coordinates
-#import seaborn as sns
+import seaborn as sns
 
 
 
@@ -78,6 +78,13 @@ def exploration(data_raw, data_all):
     df_stats = data_all.groupby('machineNumber').mean().reset_index()
     df_stats['temperature_mean'] = (df_stats['TempSensor0']+df_stats['TempSensor1']+df_stats['TempSensor2']+df_stats['TempSensor3'])/4
     df_stats['vibration_mean'] = (df_stats['VibraSensor0']+df_stats['VibraSensor1']+df_stats['VibraSensor2']+df_stats['VibraSensor3'])/4
+    
+    
+    df_stats['maintenance'] = ['urgent' if r['temperature_mean']>72 or  r['vibration_mean']>1650 
+                               else 'short' if r['temperature_mean']>70 or  r['vibration_mean']>1600 
+                               else 'long' 
+                               for i,r in df_stats.iterrows()]
+    
     df_plot_t = df_stats[["machineNumber","TempSensor0","TempSensor1","TempSensor2","TempSensor3"]]
     df_plot_v = df_stats[["machineNumber","VibraSensor0","VibraSensor1","VibraSensor2","VibraSensor3"]]
     data_plot = {'temperature':df_plot_t,'vibration':df_plot_v}    
@@ -93,7 +100,7 @@ def exploration(data_raw, data_all):
         fig, ax = plt.subplots(figsize=(10, 6))
         parallel_coordinates(
             data_plot[dim],
-            class_column='machineNumber',   # definiert die Farben
+            class_column='machineNumber',   # define color
             colormap='viridis',
             ax = ax
         )
@@ -116,21 +123,20 @@ def exploration(data_raw, data_all):
     #-----------------------------------------------
     # scatter plot
     output_dir = "plots"
-    name_pattern = "00_exploration_"    
-    
+    name_pattern = "00_exploration_"        
         
     order = df_stats['machineNumber'].dropna().unique()
     ax = sns.scatterplot(
         data=df_stats,
-        x='temperature_mean',
-        y='vibration_mean',
-        hue='machineNumber',        # Farbe
+        x='vibration_mean',
+        y='temperature_mean',
+        hue='machineNumber',        # color
         hue_order = order,
         palette=sns.color_palette("husl", len(order))
-    ) 
-    
+    )
+        
     # set title
-    title = f'scatter plot - vibration vs temperature - mean values'
+    title = 'scatter plot - temperature over vibration - mean values'
     ax.set_title(title)
     
     # legend outside of plot area
@@ -144,6 +150,22 @@ def exploration(data_raw, data_all):
     plt.show()
     
     return True
+
+def categorize_by_mean(data):
+        
+    df_stats = data.groupby('machineNumber').mean().reset_index()
+    df_stats['temperature_mean'] = (df_stats['TempSensor0']+df_stats['TempSensor1']+df_stats['TempSensor2']+df_stats['TempSensor3'])/4
+    df_stats['vibration_mean'] = (df_stats['VibraSensor0']+df_stats['VibraSensor1']+df_stats['VibraSensor2']+df_stats['VibraSensor3'])/4
+    
+    
+    df_stats['maintenance'] = ['urgent' if r['temperature_mean']>72 or  r['vibration_mean']>1650 
+                              else 'short' if r['temperature_mean']>70 or  r['vibration_mean']>1600 
+                              else 'long' 
+                              for i,r in df_stats.iterrows()]
+    
+    return df_stats[['machineNumber','maintenance']]
+
+
 
 
 #%%
@@ -161,9 +183,56 @@ if __name__ == '__main__':
     exploration(data_raw, data_all)
     
    
+    #-----------------------------------------------
+    # categorize by mean value
+    #-----------------------------------------------
+    labeled_by_mean = categorize_by_mean(data_all)
     
-   
+    #-----------------------------------------------
+    # scatter plot
+    df_stats = data_all.groupby('machineNumber').mean().reset_index()
+    df_stats['temperature_mean'] = (df_stats['TempSensor0']+df_stats['TempSensor1']+df_stats['TempSensor2']+df_stats['TempSensor3'])/4
+    df_stats['vibration_mean'] = (df_stats['VibraSensor0']+df_stats['VibraSensor1']+df_stats['VibraSensor2']+df_stats['VibraSensor3'])/4
     
+    # join labels
+    df_stats.set_index('machineNumber', inplace=True)
+    labeled_by_mean.set_index('machineNumber', inplace=True)
+    df_plot = pd.concat([df_stats,labeled_by_mean], axis=1)
+    df_plot.reset_index(inplace=True)
+    
+    
+    output_dir = "plots"
+    name_pattern = "01_labeling_"        
+        
+    color_map = {
+        'urgent': 'red',
+        'short': 'orange',
+        'long': 'green'
+        }
+    
+    order = df_plot['maintenance'].dropna().unique()
+    ax = sns.scatterplot(
+        data=df_plot,
+        x='vibration_mean',
+        y='temperature_mean',
+        hue='maintenance',        # color
+        hue_order = order,
+        palette=color_map#sns.color_palette("husl", len(order))
+    )
+        
+    # set title
+    title = 'labeled data - temperature over vibration - mean values'
+    ax.set_title(title)
+    
+    # legend outside of plot area
+    ax.legend(
+        loc='center left',
+        bbox_to_anchor=(1.02, 0.5),
+        title='maintenance'
+    )
+    plt.tight_layout()
+    plt.savefig(f"{output_dir}\{name_pattern}_{title}.png")
+    plt.show()
    
     
 
